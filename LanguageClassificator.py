d6c8d070d4ae3c32e0952ee50420ed1f03b0770b
mikipacman <miki.pacman@gmail.com>
2019-06-04 23:00:10 +0200

    no coś tam wykrywa

--- LanguageClassificator.py
+++ LanguageClassificator.py
@@ -1,32 +1,79 @@
 from os import listdir
 from string import ascii_lowercase
 from os.path import join, isdir, isfile
+from SigmoidPerceptron import SigmoidPerceptron
+import numpy as np
 
-# Data preparation
-mypath = "datasets/"
-dirs = [f for f in listdir(mypath) if isdir(join(mypath, f))]
+# Params
+alpha = 0.01
+beta = 0.01
+iterations = 2500
 
-x = []
-y = []
 
-for d in dirs:
-    y.append(d)
+# Util functions
+def get_ascii_stat(s):
     letters = {}
     for i in ascii_lowercase:
         letters[i] = 0
 
-    files = [f for f in listdir(mypath + d) if isfile(join(mypath + d, f))]
-    for file_name in files:
-        f = open(join(mypath + d, file_name))
-        s = f.read()
-        s = s.lower()
-        for i in ascii_lowercase:
-            letters[i] += s.count(i)
+    for i in ascii_lowercase:
+        letters[i] += s.count(i)
 
     letters_list = list(letters.values())
     sum_letters = sum(letters_list)
-    letters_list = [i / sum_letters for i in letters_list]
-    x.append(letters_list)
+    return [i / sum_letters for i in letters_list]
 
-print(x)
-print(y)
+
+def get_lang(s):
+    score = [x.predict(np.array(get_ascii_stat(s))) for x in p]
+    maximum = 0
+    best = ""
+    for i in range(0, len(score)):
+        print("%s: %f" % (languages[i], score[i]))
+        if maximum < score[i]:
+            maximum = score[i]
+            best = languages[i]
+
+    print("prediction: %s\n" % best)
+
+
+# Data preparation
+mypath = "datasets/"
+languages = [f for f in listdir(mypath) if isdir(join(mypath, f))]
+
+x_texts = []
+y_lang = []
+
+for d in languages:
+
+    files = [f for f in listdir(mypath + d) if isfile(join(mypath + d, f))]
+    for file_name in files:
+
+        f = open(join(mypath + d, file_name))
+        s = f.read()
+        s = s.lower()
+        n = 100
+        parts = [s[i:i + n] for i in range(0, len(s), n)]
+        for part in parts:
+            x_texts.append(np.array(get_ascii_stat(part)))
+            y_lang.append(d)
+
+
+# Init perceptrons
+p = [SigmoidPerceptron(len(ascii_lowercase)) for i in range(0, len(languages))]
+
+# Train perceptrons 'one against every other'
+for i in range(0, len(languages)):
+
+    y_decision = []
+    for j in range(0, len(x_texts)):
+        y_decision.append(int(languages[i] == y_lang[j]))
+
+    print("teaching perceptron #%d (%s)" % (i, languages[i]))
+    p[i].teach_on_dataset(x_texts, y_decision, alpha, beta, iterations)
+    print()
+
+# Testing
+while True:
+    s = input("type word: ")
+    get_lang(s)
--- SigmoidPerceptron.py
+++ SigmoidPerceptron.py
@@ -10,24 +10,16 @@ class SigmoidPerceptron:
     def predict(self, x):
         return 1 / (1 + np.exp(-sum(x * self.w) + self.b))
 
-    def grad_w(self, x, y):
-        p = self.predict(x)
-        return (p - y) * p * (1 - p) * x
-
-    def grad_b(self, x, y):
-        p = self.predict(x)
-        return (p - y) * p * (1 - p)
-
     def train(self, x, y, alpha, beta):
         p = self.predict(x)
-        self.w = self.w - alpha * self.grad_w(x, y)
-        self.b = self.b - beta * self.grad_b(x, y)
+        self.w = self.w + alpha * (y - p) * x
+        self.b = self.b + beta * (p - y)
 
     def err(self, x, y):
         return 0.5 * (self.predict(x) - y) ** 2
 
     def teach_on_dataset(self, x, y, alpha, beta, iterations):
-        n = x.shape[0]
+        n = len(x)
 
         for j in range(0, iterations):
             square_err = 0
@@ -37,4 +29,5 @@ class SigmoidPerceptron:
             for i in range(0, n):
                 square_err += self.err(x[i], y[i])
 
-            print(square_err)
+            if j % (iterations / 10) == 0:
+                print(square_err)
